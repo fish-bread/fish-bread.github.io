0d68462855cd3106ad638dd4c9763bedabdc8bc7
fishbread <123308170+3434241933@users.noreply.github.com>
2025-08-10 17:34:25 +0800

    v1.0.0-alpha-改进

--- docs/.vuepress/.temp/internal/searchIndex.js
+++ docs/.vuepress/.temp/internal/searchIndex.js
@@ -7,6 +7,49 @@ export const SEARCH_INDEX = [
     "extraFields": []
   },
   {
+    "title": "live2d预览",
+    "headers": [],
+    "path": "/posts/Resource/live2d.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "资源相关",
+    "headers": [],
+    "path": "/posts/Resource/resource.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "主题切换",
+    "headers": [],
+    "path": "/posts/Resource/themeChoose.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "puppeteer",
+    "headers": [
+      {
+        "level": 2,
+        "title": "什么是 Puppeteer？",
+        "slug": "什么是-puppeteer",
+        "link": "#什么是-puppeteer",
+        "children": []
+      },
+      {
+        "level": 2,
+        "title": "Puppeteer特性",
+        "slug": "puppeteer特性",
+        "link": "#puppeteer特性",
+        "children": []
+      }
+    ],
+    "path": "/posts/Puppeteer/puppeteer.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
     "title": "koa文档",
     "headers": [
       {
@@ -32,27 +75,6 @@ export const SEARCH_INDEX = [
       }
     ],
     "path": "/posts/Koa/koa.html",
-    "pathLocale": "/",
-    "extraFields": []
-  },
-  {
-    "title": "live2d预览",
-    "headers": [],
-    "path": "/posts/Resource/live2d.html",
-    "pathLocale": "/",
-    "extraFields": []
-  },
-  {
-    "title": "资源相关",
-    "headers": [],
-    "path": "/posts/Resource/resource.html",
-    "pathLocale": "/",
-    "extraFields": []
-  },
-  {
-    "title": "主题切换",
-    "headers": [],
-    "path": "/posts/Resource/themeChoose.html",
     "pathLocale": "/",
     "extraFields": []
   },
@@ -118,28 +140,6 @@ export const SEARCH_INDEX = [
       }
     ],
     "path": "/posts/Vue/%E5%A6%82%E4%BD%95%E5%9C%A8vue3%E5%BC%95%E5%85%A5live2d.html",
-    "pathLocale": "/",
-    "extraFields": []
-  },
-  {
-    "title": "puppeteer",
-    "headers": [
-      {
-        "level": 2,
-        "title": "什么是 Puppeteer？",
-        "slug": "什么是-puppeteer",
-        "link": "#什么是-puppeteer",
-        "children": []
-      },
-      {
-        "level": 2,
-        "title": "Puppeteer特性",
-        "slug": "puppeteer特性",
-        "link": "#puppeteer特性",
-        "children": []
-      }
-    ],
-    "path": "/posts/Puppeteer/puppeteer.html",
     "pathLocale": "/",
     "extraFields": []
   },
