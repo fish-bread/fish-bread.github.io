279b611be42b53cbc18da5784227d6f2b80dbf94
fishbread <3434241933@qq.com>
2025-09-29 11:26:46 +0800

    小更新

--- docs/.vuepress/theme/components/download/downloadIndex.vue
+++ docs/.vuepress/theme/components/download/downloadIndex.vue
@@ -6,8 +6,8 @@ import {ref} from "vue";
 const AppList = ref<AListInter>({
   title: '工具软件(仅限windows)',
   linkArray: [
-      { href: 'http://localhost:8080/header/124017298_p0.png', h2title: '图片链接', hrefName: '图片链接', note: '备注:' },
-    { href: 'http://localhost:8080/download/electron-python-1.0.0-setup.exe', h2title: '个人脚本gui', hrefName: 'electron-python-1.0.0-setup.exe', note: '备注:' },
+      { href: 'https://fishbread.net/header/124017298_p0.png', h2title: '图片链接', hrefName: '图片链接', note: '备注:' },
+    { href: 'https://fishbread.net/download/electron-python-1.0.0-setup.exe', h2title: '个人脚本gui', hrefName: 'electron-python-1.0.0-setup.exe', note: '备注:' },
   ]
 })
 </script>
